5b62dcef31a55a90a39643c97ef9ec932aab439e
AARQ-HO <56830579+AARQ-HO@users.noreply.github.com>
2021-05-04 10:51:55 +0800

    改為使用 委買 委賣作圖

--- main.js
+++ main.js
@@ -35,8 +35,8 @@ function showList(list)
                 '<td>最高</td>'+
                 '<td>最低</td>'+
                 '<td>類股</td>'+
-                '<td>內盤</td>'+
-                '<td>外盤</td>'+
+                '<td>賣單</td>'+
+                '<td>買單</td>'+
                 '<td>操作</td>'+
                 '</tr></table>');
 				
@@ -57,8 +57,23 @@ function showList(list)
 							var chartIn = $('<div class="chart inMarket"></div>');
 							var chartOut = $('<div class="chart outMarket"></div>');
 							/* 長條圖 */
-							chartIn.css('width',data[0].inMarketPercentage);
-							chartOut.css('width',data[0].outMarketPercentage);
+                            /*內盤 外盤*/
+                            /*
+                            inMarket
+                            inMarketPercentage
+                            outMarket
+                            outMarketPercentage
+                            */
+                            /**/
+
+                            /* 委買 委賣*/
+                            var buyRate = (data[0].sumBidVolK / (data[0].sumBidVolK + data[0].sumAskVolK))*100;
+                            buyRate = buyRate.toFixed(2);
+                            var sellRate = 100 - buyRate;
+                            sellRate = sellRate.toFixed(2);
+                            /**/
+							chartIn.css('width',sellRate+'%');
+							chartOut.css('width',buyRate+'%');
                             var itemobj = '<tr>'+
                                 '<td>'+item+'<br>'+data[0].symbolName+'</td>'+
                                 '<td>'+data[0].price+'</td>'+
@@ -66,7 +81,7 @@ function showList(list)
                                 '<td>'+data[0].regularMarketDayHigh+'</td>'+
                                 '<td>'+data[0].regularMarketDayLow+'</td>'+
                                 '<td>'+data[0].sectorName+'</td>'+
-                                '<td colspan="2" style="width:200px" id="'+ chartId +'" ><div class="text-wrap"><div class="text inMarket">'+data[0].inMarket+' ('+data[0].inMarketPercentage+')</div><div class="text outMarket">'+data[0].outMarket+' ('+data[0].outMarketPercentage+')</div></div>'+
+                                '<td colspan="2" style="width:200px" id="'+ chartId +'" ><div class="text-wrap"><div class="text inMarket">'+data[0].sumAskVolK+' ('+sellRate+')</div><div class="text outMarket">'+data[0].sumBidVolK+' ('+buyRate+')</div><br></div>'+
                                 '<td><input id="'+clickId+'" type="button" value="X"></td>>'
                                 +'</tr>>'
                             ;
